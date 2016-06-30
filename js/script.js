2c9c60ccd80e02442db67fcc6845483190f4cdb3
Vincent <vincentnewkirk@gmail.com>
2016-06-29 15:23:02 -1000

    it works!! works with duplicate number elements and zeroes

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 var partition = function( arr ){
-  var pivot;
+  var pivot = [];
   var counter;
   var partitionL;
   var partitionR;
   if(arr.length <= 1){
     return arr;
   }
-  pivot = arr.pop();
+  pivot.push(arr.pop());
 
   partitionL = arr.filter( function( element ) {
     return element <= pivot;
@@ -14,14 +14,9 @@ var partition = function( arr ){
   partitionR = arr.filter( function( element ) {
     return element > pivot;
   });
-  console.log(partitionL, 'L');
-  console.log(partitionR, 'R');
-  var completedSort = partition(partitionL) + pivot + partition(partitionR);
-  var answer = completedSort.split();
-  return answer;
+  var completedSort = partition(partitionL).concat(pivot, partition(partitionR));
+  return completedSort;
 
 };
-var someArr = [3,7,1,9,5,8,6,4,7,8,9,2,5,7,3,2];
-console.log(partition(someArr));
 
-module.exports = quickSort;
+module.exports = partition;
--- test/test.spec.js
+++ test/test.spec.js
@@ -1,20 +1,22 @@
 var chai = require('chai');
 var should = chai.should();
 var expect = chai.expect;
-var mergeSort = require('./../js/script.js');
+var quickSort = require('./../js/script.js');
 
 describe('Merge Sort', function () {
   it('Should exist', function () {
-    expect(mergeSort).to.exist;
+    expect(quickSort).to.exist;
   });
   it('Should be a function', function () {
-    expect(mergeSort).to.be.a('function');
+    expect(quickSort).to.be.a('function');
   });
 
   describe('The Result', function(){
     var arr= [3,7,1,9,5,6,4,8,8,];
+    var arr2 = [5,0,0,4,4,5,5,6,6,1,1,29,20,3,65];
     it('Should sort the array', function () {
-      expect(mergeSort(arr)).to.deep.equal([1,3,4,5,6,7,8,8,9]);
+      expect(quickSort(arr)).to.deep.equal([1,3,4,5,6,7,8,8,9]);
+      expect(quickSort(arr2)).to.deep.equal([0,0,1,1,3,4,4,5,5,5,6,6,20,29,65]);
     });
   });
 });
